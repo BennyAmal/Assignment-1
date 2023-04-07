28b7e48c75fc15564171810ba3866b4f5f339830
Amal Benny <amalbennymunjanattu@gmail.com>
2023-04-07 21:22:59 +0530

    Renamed the Variables

--- Assignment 1/Assignment 1/Assignment_1.py
+++ Assignment 1/Assignment 1/Assignment_1.py
@@ -1,16 +1,16 @@
 print("this is a program to count the uppercase and lower case letters in a given string")
 def lettercount(s):                                   #Defining funtion
-    a = 0
-    b = 0
+    uppernos = 0
+    lowernos = 0
     
     for char in s:
         if char.isupper():  
-            a=a+1
+            uppernos=uppernos+1
         elif char.islower():
-            b=b+1
+            lowernos=lowernos+1
     
-    print("Number of uppercase letters:", a)
-    print("Number of lowercase letters:", b)
+    print("Number of uppercase letters:", uppernos)
+    print("Number of lowercase letters:", lowernos)
 
 s =input("enter your string ")
 lettercount(s)                                        #invoking funtion
