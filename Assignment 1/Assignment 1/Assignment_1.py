8eb9c8fe4a81c29d9fae3c59dbb0164fabe4caf1
Amal Benny <amalbennymunjanattu@gmail.com>
2023-04-07 20:28:40 +0530

    Update Assignment_1.py

--- Assignment 1/Assignment 1/Assignment_1.py
+++ Assignment 1/Assignment 1/Assignment_1.py
@@ -1,18 +1,18 @@
 print("this is a program to count the uppercase and lower case letters in a given string")
-def lettercount(s):    #Defining funtion
+def lettercount(s):                                   #Defining funtion
     a = 0
     b = 0
     
     for letter in s:
         if letter.isupper():  
-            a += 1
+            a=a+1
         elif letter.islower():
-            b += 1
+            b=b+1
     
     print("Number of uppercase letters:", a)
     print("Number of lowercase letters:", b)
 
 s =input("enter your string ")
-lettercount(s)    #invoking funtion
+lettercount(s)                                        #invoking funtion
 print("CODED BY AMAL BENNY")
 
